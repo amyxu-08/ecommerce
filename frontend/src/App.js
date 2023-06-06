a7a26cd957a5700b20ce8fc896330f941b07956e
amyxu <amyxu@berkeley.edu>
2023-06-06 13:34:32 -0700

    Elements Bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { Box } from "@mui/system";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Components1/Home/Home";
+import Payment from "./Components1/Payment/Payment";
+import Completion from "./Components1/Payment/Completion";
 import NavigationBar from "./Components1/Navigation/NavigationBar";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 
@@ -31,6 +33,8 @@ function App() {
         <Box>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/payment" element={<Payment />} />
+            <Route path="/completion" element={<Completion />} />
           </Routes>
         </Box>
       </div>
--- frontend/src/Components1/Payment/Payment.js
+++ frontend/src/Components1/Payment/Payment.js
@@ -4,30 +4,35 @@ import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "./CheckoutForm";
 import { loadStripe } from "@stripe/stripe-js";
 
+import { Box } from "@mui/material";
+
 function Payment() {
   const [stripePromise, setStripePromise] = useState(null);
   const [clientSecret, setClientSecret] = useState("");
 
   useEffect(() => {
-    fetch("/config").then(async (r) => {
+    fetch("http://localhost:9000/server/config").then(async (r) => {
       const { publishableKey } = await r.json();
       setStripePromise(loadStripe(publishableKey));
+      console.log(stripePromise);
     });
   }, []);
 
   useEffect(() => {
-    fetch("/create-payment-intent", {
+    fetch("http://localhost:9000/server/create-payment-intent", {
       method: "POST",
       body: JSON.stringify({}),
     }).then(async (result) => {
       var { clientSecret } = await result.json();
       setClientSecret(clientSecret);
+      console.log(clientSecret);
     });
   }, []);
 
   return (
     <>
-      <h1>React Stripe and the Payment Element</h1>
+      <Box height={70}></Box>
+      <h1>Checkout Now 💵</h1>
       {clientSecret && stripePromise && (
         <Elements stripe={stripePromise} options={{ clientSecret }}>
           <CheckoutForm />
