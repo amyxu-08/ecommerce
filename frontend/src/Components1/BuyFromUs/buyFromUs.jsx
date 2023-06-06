5cab85da8855c93868bf2dae9e80b7588a53a333
ZaidFada231 <zaidfada01@gmail.com>
2023-06-06 14:03:42 -0400

    added filter, and better display

--- frontend/src/Components1/BuyFromUs/buyFromUs.jsx
+++ frontend/src/Components1/BuyFromUs/buyFromUs.jsx
@@ -7,17 +7,24 @@ import {
   CardMedia,
   CardContent,
   Typography,
-  Select,
-  MenuItem,
-  FormControl,
-  InputLabel,
+  Button,
+  Tabs,
+  Tab,
 } from "@mui/material";
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect } from "react";
+
+function formatTitle(category) {
+  const words = category.split("-");
+  return words
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
 
 function BuyFromUs() {
   const API_URL = "http://localhost:9000";
 
-  const [categoryData, setCategoryData] = useState([]);
+  const [categoryData, setCategoryData] = useState({});
+  const [selectedCategory, setSelectedCategory] = useState("all");
 
   const fetchData = async (category) => {
     try {
@@ -25,8 +32,10 @@ function BuyFromUs() {
       const data = await response.json();
       console.log(data);
 
-      // Update the categoryData state with the fetched data
-      setCategoryData((prevData) => [...prevData, data.categoryData.products]);
+      setCategoryData((prevData) => ({
+        ...prevData,
+        [category]: data.categoryData.products,
+      }));
     } catch (error) {
       console.error("Error retrieving category data:", error);
     }
@@ -46,42 +55,106 @@ function BuyFromUs() {
   ];
 
   useEffect(() => {
-    // Fetch data for each category
-    categories.forEach((category) => {
-      fetchData(category);
-    });
+    const fetchDataForCategories = async () => {
+      const promises = categories.map((category) => fetchData(category));
+
+      try {
+        await Promise.all(promises);
+      } catch (error) {
+        console.error("Error retrieving category data:", error);
+      }
+    };
+
+    fetchDataForCategories();
   }, []);
 
-  // Render your component JSX using the categoryData state
+  const filterByCategory = (category) => {
+    if (selectedCategory === "all") {
+      return true;
+    } else if (selectedCategory === "womens") {
+      return (
+        category.toLowerCase().includes("womens") ||
+        category === "sunglasses" ||
+        category === "tops"
+      );
+    } else if (selectedCategory === "mens") {
+      return (
+        category.toLowerCase().startsWith("mens-") || category === "sunglasses"
+      );
+    }
+  };
+
+  const handleCategoryChange = (event, newCategory) => {
+    setSelectedCategory(newCategory);
+  };
+
   return (
     <>
       <CssBaseline />
+      <br />
+      <br />
+      <br />
       <Container maxWidth="lg">
-        <Grid container spacing={4}>
-          {categoryData.map((products, index) => (
-            <React.Fragment key={index}>
-              {products.map((data) => (
-                <Grid item xs={12} sm={6} md={4} lg={3} key={data.id}>
-                  <Card style={{ height: "100%" }}>
-                    <CardMedia
-                      style={{ paddingTop: "56.35%" }}
-                      image={data.thumbnail}
-                      title={data.title}
-                    />
-                    <CardContent style={{ flexGrow: 1 }}>
-                      <Typography variant="h6" component="div" gutterBottom>
-                        {data.title}
-                      </Typography>
-                      <Typography variant="body2" color="textSecondary">
-                        Description: {data.description}
-                      </Typography>
-                    </CardContent>
-                  </Card>
+        <Tabs
+          value={selectedCategory}
+          onChange={handleCategoryChange}
+          variant="fullWidth"
+        >
+          <Tab value="all" label="All" />
+          <Tab value="womens" label="Womens" />
+          <Tab value="mens" label="Mens" />
+        </Tabs>
+
+        {categories.map((category) => (
+          <React.Fragment key={category}>
+            {filterByCategory(category) && (
+              <>
+                <br />
+                <Typography variant="h5" component="h2" gutterBottom>
+                  {formatTitle(category)}
+                </Typography>
+                <Grid container spacing={4}>
+                  {categoryData[category] &&
+                    categoryData[category].slice(0, 4).map((data) => (
+                      <Grid item xs={12} sm={6} md={4} lg={3} key={data.id}>
+                        <Card
+                          style={{
+                            height: "100%",
+                            display: "flex",
+                            flexDirection: "column",
+                          }}
+                        >
+                          <CardMedia
+                            style={{ paddingTop: "56.35%" }}
+                            image={data.thumbnail}
+                            title={data.title}
+                          />
+                          <CardContent style={{ flexGrow: 1 }}>
+                            <Typography
+                              variant="h6"
+                              component="div"
+                              gutterBottom
+                            >
+                              {data.title}
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary">
+                              Price: {data.price}$ | Rating: {data.rating} ★{" "}
+                            </Typography>
+                            <br />
+                          </CardContent>
+                          <div style={{ padding: "8px", marginTop: "auto" }}>
+                            <Button variant="contained" color="primary">
+                              Add to Cart
+                            </Button>
+                          </div>
+                        </Card>
+                      </Grid>
+                    ))}
                 </Grid>
-              ))}
-            </React.Fragment>
-          ))}
-        </Grid>
+              </>
+            )}
+          </React.Fragment>
+        ))}
       </Container>
     </>
   );
