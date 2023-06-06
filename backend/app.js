157279d7ab96f839f41e71c0cf0088b1fcbac5f3
itskevinshi <kevinshi0@outlook.com>
2023-06-06 11:52:54 -0400

    added public api for all items and specific items

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 require("dotenv").config();
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var publicRouter = require("./routes/publicapi/public");
 
 var app = express();
 
@@ -35,6 +36,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/public", publicRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
