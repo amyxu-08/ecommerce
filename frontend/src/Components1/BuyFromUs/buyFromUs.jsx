071adf1a6b65824aecd1692b5876d9481f0300b4
ZaidFada231 <zaidfada01@gmail.com>
2023-06-07 08:47:40 -0400

    changed grid

--- frontend/src/Components1/BuyFromUs/buyFromUs.jsx
+++ frontend/src/Components1/BuyFromUs/buyFromUs.jsx
@@ -154,7 +154,7 @@ function BuyFromUs() {
                       {filteredProducts(category)
                         ?.slice(0, 4)
                         .map((data) => (
-                          <Grid item xs={12} sm={6} md={4} lg={3} key={data.id}>
+                          <Grid item xs={12} sm={6} md={3} lg={3} key={data.id}>
                             <Card
                               style={{
                                 height: "100%",
